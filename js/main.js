43771dc52e7e6a3b89182b137ecd42c0e82e3a06
Pablo Venica <pablo.venica7@gmail.com>
2025-04-30 18:02:24 -0300

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+document.getElementById('abrirFiltros').addEventListener('click', () => {
+    document.getElementById('modalFiltros').style.display = 'flex';
+  });
+  
+  document.getElementById('cerrarFiltros').addEventListener('click', () => {
+    document.getElementById('modalFiltros').style.display = 'none';
+  });
+  
+  document.getElementById('cerrarFiltros2').addEventListener('click', () => {
+    document.getElementById('modalFiltros').style.display = 'none';
+  });
+  
