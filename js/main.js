6aa1d26061ffaf41a9035d72212875de700c62da
Pablo Venica <pablo.venica7@gmail.com>
2025-05-03 11:52:37 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
+// Abrir el modal de filtros al hacer clic en el botón
 document.getElementById('abrirFiltros').addEventListener('click', () => {
-    document.getElementById('modalFiltros').style.display = 'flex';
-  });
-  
-  document.getElementById('cerrarFiltros').addEventListener('click', () => {
+  document.getElementById('modalFiltros').style.display = 'flex';
+});
+
+// Cerrar el modal al hacer clic en el botón "Cancelar" del pie del modal
+document.getElementById('cerrarFiltros2').addEventListener('click', () => {
+  document.getElementById('modalFiltros').style.display = 'none';
+});
+
+// Cerrar el modal si se presiona la tecla ESC
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
     document.getElementById('modalFiltros').style.display = 'none';
-  });
-  
-  document.getElementById('cerrarFiltros2').addEventListener('click', () => {
-    document.getElementById('modalFiltros').style.display = 'none';
-  });
-  
+  }
+});
